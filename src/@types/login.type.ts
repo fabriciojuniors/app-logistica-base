06ab3bf06c0da276ca01514293533cd54402272e
Fabricio Junior (Senac) <fabriciojuniorsc@gmail.com>
2025-11-10 21:27:01 -0300

    add type login

---
+++ src/@types/login.type.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+const loginSchema = z.object({
+    email: z.email("O e-mail informado é inválido!"),
+    senha: z.string()
+        .min(3, "Deve possuir ao menos 3 caracteres")
+        .max(20, "Deve possuir no máximo 20 caracteres"),
+});
+
+export type Login = z.infer<typeof loginSchema>
